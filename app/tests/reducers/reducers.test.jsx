ff2ab85dd8b5ba6386b8ecb6959518375a9f0169
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-05-04 15:32:11 -0400

    todosReducer and test

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,6 +1,7 @@
 var expect = require('expect');
 var df = require('deep-freeze-strict');
 var reducers = require('reducers');
+var moment = require('moment');
 
 
 describe('reducers', () => {
@@ -27,5 +28,43 @@ describe('reducers', () => {
 
 			expect(res).toEqual(true);
 		});
-	})
+	});
+
+	describe('todosReducer', () => {
+		it('should add new todo', () => {
+			var action = {
+				type: 'ADD_TODO',
+				text: 'test todo'
+			};
+
+			var res = reducers.todosReducer(df([]), df(action));
+
+			expect(res.length).toEqual(1); //looking at length to see if todo array was updated
+			expect(res[0].text).toEqual(action.text);
+		});
+
+
+		it('should toggleTodo and toggle completed and change completedAt', () => {
+			var todos = [{
+					id: '123',
+					text: 'stuff to do',
+					completed: true,
+					createdAt: 123,
+					completedAt: 125
+			}];
+
+			var action = {
+				type: 'TOGGLE_TODO',
+				id: '123'
+			};
+
+			var res = reducers.todosReducer(df(todos), df(action));
+
+			expect(res[0].completed).toEqual(false);
+			expect(res[0].completedAt).toEqual(undefined);
+
+		});
+	});
+
+
 });
